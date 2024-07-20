6085c24ace76e6845b86f03a2b433d390f41a7a0
kanta0609 <kantakomuro0609@gmail.com>
2024-07-20 23:58:09 +0900

    背景色を追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -42,13 +43,13 @@ function App() {
   const today = getDate();
   return (
     <>
-      <h1>TO DO LIST</h1>
-      <div>{today}</div>
+      <h1 className="title">TO DO LIST</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button onClick={handleClear}>完了したタスクの削除</button>
       <div>残りのタスク数:{todos.filter((todo) => !todo.completed).length}</div>
+      <div className="date">{today}</div>
     </>
   );
 }
